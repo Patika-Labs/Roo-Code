cf870deadb8b4e79e3666426096c9ae9255514f9
Hasan Chebi <hsnchebi@gmail.com>
2025-09-16 02:17:14 +0200

    convert cjs to esm

---
+++ src/_api.ts
@@ -0,0 +1,3 @@
+export * from "./api"
+export * from "./api/transform/stream"
+export { type ModelInfo } from "@roo-code/types"
--- src/dist/core/prompts/__tests__/utils.d.ts
+++
@@ -1,3 +0,0 @@
-import * as fs from "fs/promises";
-import { PathLike } from "fs";
-export declare function toPosix(filePath: PathLike | fs.FileHandle): string;
